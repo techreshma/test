8da58cd7bc267818516f22f6419e43ce0eea7e3f
techreshma <reshma.jayaprakash@techcarrot.ae>
2022-05-05 15:59:42 +0400

    menu task

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -7,6 +7,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
-{
+{  
+    public $table = "menu_items";
 
+    public function parent() {
+        return $this->belongsToOne(static::class, 'parent_id ');
+      }
+      
+    public function children() {
+        return $this->hasMany(static::class, 'parent_id')->orderBy('name', 'asc');
+      }  
 }
